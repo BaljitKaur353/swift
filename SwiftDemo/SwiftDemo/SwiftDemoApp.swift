4333431797c693d1eb75df347048d476729d8bcc
baljitkaur353 <Goraya@1996>
2025-09-14 16:19:25 +0530

    Updated code

---
+++ SwiftDemo/SwiftDemo/SwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoApp.swift
+//  SwiftDemo
+//
+//  Created by Baljit  Goraya on 14/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
